620d0e6526bcd38f46c09875926f0d82c55ce828
Nicole Mark <nicolelily@icloud.com>
2025-04-23 14:38:59 -0400

    modified:   .DS_Store 	modified:   sda/.DS_Store 	modified:   sda/main.ts 	new file:   sda/output/chart.png

--- sda/main.ts
+++ sda/main.ts
@@ -1,4 +1,5 @@
 import { SimpleDB } from "@nshiab/simple-data-analysis";
+import { barX, plot } from "@observablehq/plot";
 
 const sdb = new SimpleDB({ cacheVerbose: true });
 
@@ -23,6 +24,43 @@ await provinces.logTable();
 const firesInsideProvinces = await fires.joinGeo(provinces, "inside", {
   outputTable: "firesInsideProvinces",
 });
+
+await firesInsideProvinces.summarize({
+  values: "hectares",
+  categories: "nameEnglish",
+  summaries: ["count", "sum"],
+  decimals: 0,
+});
+
+await firesInsideProvinces.renameColumns({
+  count: "numberFires",
+  sum: "burntArea",
+});
+await firesInsideProvinces.sort({ burntArea: "desc" });
 await firesInsideProvinces.logTable();
+
+// await firesInsideProvinces.logBarChart("nameEnglish", "burntArea");
+
+const chart = (data: unknown[]) =>
+  plot({
+    marginLeft: 170,
+    grid: true,
+    x: { tickFormat: (d) => `${d / 1_0000_000}M`, label: "Burnt area (ha)" },
+    y: { label: null },
+    color: { scheme: "Reds" },
+    marks: [
+      barX(data, {
+        x: "burntArea",
+        y: "nameEnglish",
+        fill: "burntArea",
+        sort: { y: "-x" },
+      }),
+    ],
+  });
+
+await firesInsideProvinces.writeChart(
+  chart,
+  "./sda/output/chart.png",
+);
 
 await sdb.done();
